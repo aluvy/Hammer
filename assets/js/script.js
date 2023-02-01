595f0608af8a2188153af3f23347ece83bc0e19e
miok <aluvy_@naver.com>
2023-02-01 13:25:02 +0900

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,28 +9,38 @@ window.addEventListener("load", ()=>{
     list.forEach((item, idx)=>{
         let mc = new Hammer(item);
         let btn = item.querySelector(".btn_del").clientWidth * -1;
-        let myx, x;
+        let myx, x, angle;
 
         mc.on("panleft panright panup pandown tap press", function(ev) {
 
             console.log(ev, ev.type, ev.deltaX, ev.deltaY, 'angle', ev.angle, ev.angle<=-175, 'distance', ev.distance, ev.srcEvent.layerY, ev.srcEvent.offsetY);
 
+            if( ev.type == "panleft" || ev.type == "panright" || ev.type == "tap" ){
+                $(item).siblings("li").css({"transform" : "translateX(0)" });
+            }
             // "panleft"
             // "panright"
 
-            $(document).find(".list_item").css({"transform" : "translateX(0)" });
+            // $(document).find(".list_item").css({"transform" : "translateX(0)" });
 
-            myx = Number((item.style.transform).replace("translateX(", "").replace("px)", ""));
+            // myx = Number((item.style.transform).replace("translateX(", "").replace("px)", ""));
             // x = ev.deltaX + myx;
             x = ev.deltaX;
+            angle = ev.angle;
 
-            x = ( x <= btn ) ? btn : x;     // min
-            x = ( x >= 0 ) ? 0 : x;         // max
+            x = ( x <= btn ) ? btn : x;     // min (btn)
+            x = ( x >= 0 ) ? 0 : x;         // max (0)
             x = ( ev.type == "tap" && x == btn ) ? 0 : x;   // max, tab
 
             if(    ( ev.angle >= -180 && ev.angle <= -165  )
-                || ( ev.angle >= 165 && ev.angle <= 180  ) ){
+                || ( ev.angle >= 165 && ev.angle <= 180  )
+                || ( ev.angle >= -20 && ev.angle <= 20  ) ){
                 item.style.transform = `translateX(${x}px)`;
+            }
+
+            if ( ev.changedPointers[0].type == "pointerup" ) {
+                x = ( x >= btn/2 ) ? 0 : btn;
+                $(item).css({"transform" : `translateX(${x}px)` });
             }
         });
 
